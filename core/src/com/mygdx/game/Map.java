8e580fa1437749eb41b5d708462a4eb37c9d04bb
Alex Woods <alex.woods.1404@gmail.com>
2017-12-16 13:07:50 +0000

    updated map, push from school didnt go through

--- core/src/com/mygdx/game/Map.java
+++ core/src/com/mygdx/game/Map.java
@@ -37,7 +37,8 @@ public class Map {
 
 	public Map() {
 		int mapWidth = 300;
-		for(int j = 0; j < 20; j++){
+		int mapHeight = 25;
+		for(int j = 0; j < mapHeight; j++){
 			ArrayList<Block> blocks = new ArrayList<Block>();
 			for(int i = 0; i < mapWidth ;i++){
 				blocks.add(new Block(i, j, true));
@@ -45,7 +46,8 @@ public class Map {
 			//add layer to list of layers
 			layers.add(blocks);
 		}
-		for(int j = 4; j < 20; j++){
+		
+		for(int j = 4; j < mapHeight; j++){
 			for(int i = 0; i < mapWidth; i++){
 				layers.get(j).get(i).setExists(false);
 			}
@@ -100,6 +102,120 @@ public class Map {
 				layers.get(j).get(i).setExists(true);
 				layers.get(j).get(i).setSolid(true);
 			}
+		}
+		for(int j = 4; j < 8; j++){
+			layers.get(j).get(88).setExists(true);
+			layers.get(j).get(88).setSolid(true);
+		}
+		for(int i = 95; i < 102; i++){
+			layers.get(6).get(i).setExists(true);
+			layers.get(6).get(i).setSolid(false);
+		}
+		for(int i = 95; i < 102; i++){
+			layers.get(9).get(i).setExists(true);
+			layers.get(9).get(i).setSolid(false);
+		}
+		layers.get(10).get(103).setExists(true);
+		layers.get(10).get(103).setSolid(false);
+		for(int i = 95; i < 102; i++){
+			layers.get(12).get(i).setExists(true);
+			layers.get(12).get(i).setSolid(false);
+		}
+		for(int j = 4; j < 13; j++){
+			layers.get(j).get(102).setExists(true);
+			layers.get(j).get(102).setSolid(true);
+		}
+		for(int i = 107; i < 114; i++){
+			layers.get(6).get(i).setExists(true);
+			layers.get(6).get(i).setSolid(false);
+		}
+		for(int j = 4; j < 8; j++){
+			for(int i = 117; i < 119; i++){
+				layers.get(j).get(i).setExists(true);
+				layers.get(j).get(i).setSolid(true);
+			}
+		}
+		for(int j = 6; j < 12; j++){
+			for(int i = 121; i < 123; i++){
+				layers.get(j).get(i).setExists(true);
+				layers.get(j).get(i).setSolid(true);
+			}
+		}
+		for(int i = 127; i < 132; i++){
+			layers.get(6).get(i).setExists(true);
+			layers.get(6).get(i).setSolid(false);
+		}
+		for(int j = 4; j < 7; j++){
+			for(int i = 137; i < 139; i++){
+				layers.get(j).get(i).setExists(true);
+				layers.get(j).get(i).setSolid(true);
+			}
+		}
+		for(int i = 136; i < 140; i++){
+			layers.get(7).get(i).setExists(true);
+			layers.get(7).get(i).setSolid(true);
+		}
+		
+		for(int j = 9; j < 13; j++){
+			for(int i = 143; i < 145; i++){
+				layers.get(j).get(i).setExists(true);
+				layers.get(j).get(i).setSolid(true);
+			}
+		}
+		for(int i = 142; i < 146; i++){
+			layers.get(8).get(i).setExists(true);
+			layers.get(8).get(i).setSolid(true);
+		}
+		for(int j = 4; j < 7; j++){
+			for(int i = 154; i < 168; i++){
+				layers.get(j).get(i).setExists(true);
+				layers.get(j).get(i).setSolid(true);
+			}
+		}
+		for(int j = 7; j < 10; j++){
+			for(int i = 158; i < 168; i++){
+				layers.get(j).get(i).setExists(true);
+				layers.get(j).get(i).setSolid(true);
+			}
+		}
+		layers.get(13).get(163).setExists(true);
+		for(int i = 168; i < 172; i++){
+			layers.get(7).get(i).setExists(true);
+			layers.get(7).get(i).setSolid(true);
+		}
+		for(int i = 176; i < 178; i++){
+			layers.get(8).get(i).setExists(true);
+			layers.get(8).get(i).setSolid(true);
+		}
+		for(int i = 180; i < 182; i++){
+			layers.get(6).get(i).setExists(true);
+			layers.get(6).get(i).setSolid(true);
+		}
+		for(int i = 184; i < 186; i++){
+			layers.get(9).get(i).setExists(true);
+			layers.get(9).get(i).setSolid(true);
+		}
+		for(int j = 4; j < 8; j++){
+			for(int i = 190; i < 198; i++){
+				layers.get(j).get(i).setExists(true);
+				layers.get(j).get(i).setSolid(true);
+			}
+		}
+		for(int i = 201; i < 204; i++){
+			layers.get(7).get(i).setExists(true);
+			layers.get(7).get(i).setSolid(true);
+		}
+		for(int i = 211; i < 220; i++){
+			layers.get(5).get(i).setExists(true);
+			layers.get(5).get(i).setSolid(false);
+		}
+		for(int i = 208; i < 220; i++){
+			layers.get(7).get(i).setExists(true);
+			layers.get(7).get(i).setSolid(false);
+		}
+		for(int i = 214; i < 220; i++){
+			layers.get(9).get(i).setExists(true);
+			layers.get(9).get(i).setSolid(false);
 		}
 		
 	}
@@ -198,7 +314,7 @@ public class Map {
 	}
 
 	public float findRightWall(int xindex, int yindex, int limit){
-		float wallX = 10000;
+		float wallX = 100000;
 		boolean foundWall = false;
 		int i = 0;
 		while(foundWall == false && i < limit){

d7b9f4390792c63ae17287fd5faf7f8b8812c830
alexwoods14 <32737905+alexwoods14@users.noreply.github.com>
2018-02-22 18:16:06 +0000

    commenting

--- core/src/com/mygdx/map/Map.java
+++ core/src/com/mygdx/map/Map.java
@@ -41,7 +41,7 @@ public class Map {
 	public char get(int i, int j){
 		char toReturn = 'N';   //standard return variable 'N' stands for nothing
 		if(i > 0 && i < Constants.MAP_WIDTH && j > 0 && j < Constants.MAP_HEIGHT){
-			if(grid[i][j] != null) {         					//checks if that block at coordinates (i,j) is empty first to avoid null errors
+			if(grid[i][j] != null) {         	//checks that block at coordinates (i,j) is not empty
 				if(grid[i][j].getClass() == SolidBlock.class){
 					toReturn = 'S';
 				}
